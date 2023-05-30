9523618635a0dfdc30889f4e9f4ac49d8fd832c5
Paulo Campos Filho <pezaum@gmail.com>
2023-05-29 17:04:31 -0700

    Added tail.js

---
+++ tail.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(theArray) {
+  let result = [];
+  if (theArray === undefined) {
+    return;
+  } else if (theArray.length <= 1) {
+    return result;
+  }
+  for (let i = 1; i < theArray.length; i++) { // start at index 1
+    result.push(theArray[i]);
+  }
+  return result;
+};
+
+// Test Case 1
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+// Test Case: Check original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
