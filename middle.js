ccddfe447154384a69eceb2415327bead002b8bb
Paulo Campos Filho <pezaum@gmail.com>
2023-05-31 09:51:45 -0700

    Cleaned up middle.js

--- middle.js
+++ middle.js
@@ -34,14 +34,6 @@ const middle = function(arr) {
   return result;
 };
 
-/*assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
-assertArraysEqual([1, 2, 3, 4], [1, 2, 3]); // => should FAIL
-assertArraysEqual([1, 2, 4], [1, 2, 3]); // => should FAIL
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4]); // => should FAIL
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4]); // => should FAIL
-assertArraysEqual([1, 2, 3, 5], [1, 2, 3, 4]); // => should FAIL
-assertArraysEqual([0, -2, 3], [0, -2, 3]); // should PASS*/
-
 assertArraysEqual(middle([0, 1, 2]),[1]); // should PASS.
 assertArraysEqual(middle([0, 1, 2]), [1, 2]); // should FAIL.
 assertArraysEqual(middle([0, 1, 2, 3]), [1, 2]); // should PASS.
