58a45b8604f42aaffbd12d638e7277da701f8a9f
Paulo Campos Filho <pezaum@gmail.com>
2023-05-31 13:54:16 -0700

    Cleaned up findKeyByValue.js and added test cases

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -7,6 +7,24 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const findKeyByValue = function() {
-
+const findKeyByValue = function(obj, value) {
+  for (let key in obj) {
+    if (obj[key] === value) return key;
+  }  
+  return;
 };
+
+
+// Tests
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+//findKeyByValue(bestTVShowsByGenre, "The Wire");
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"); // should PASS
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined); // should PASS
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy") // should pass
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "drama") // should fail
