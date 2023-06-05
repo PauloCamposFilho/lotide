0e103c5fd781d155d64ff575b7d80a3467278bf1
Paulo Campos Filho <pezaum@gmail.com>
2023-06-05 13:07:21 -0700

    Refactored tailTest.js to use Mocha&Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,11 @@
-const assertEqual = require("../assertEqual");
+const { assert } = require("chai");
 const tail = require("../tail");
 
-// Test Case 1
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-// Test Case: Check original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("given [\"Hello\", \"Lighthouse\", \"Labs\"] returns [ \"Lighthouse\", \"Labs\" ]", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it("given only one item in array, returns empty array", () => {
+    assert.deepEqual(tail(["Lighthouse"]), []);
+  });
+});
